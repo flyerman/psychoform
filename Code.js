963b4f92df491b4b59b7be15e9a2b49cf990d671
Florent Guiliani <florent@Shannons-MBP.fios-router.home>
2020-10-31 15:29:10 -0400

    initial skeleton

---
+++ Code.js
@@ -0,0 +1,13 @@
+function onFormSubmit(event) {
+    
+    // Locate the drive folder
+    var folder = DriveApp.getFolderById('1l1PNYtABFmZyZTI-4gqLGNyg1OwNMqm7');
+
+    // Create a new report
+    var report = DocumentApp.create('Report assessment');
+
+    // Move the report into the drive folder
+    var file = DriveApp.getFileById(report.getId());
+    folder.addFile(file);
+    DriveApp.getRootFolder().removeFile(file);
+}
