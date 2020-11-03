2cb2006400352826f74118183b69d9a8eceed425
Florent Guiliani <florent@guiliani.fr>
2020-11-03 06:53:08 -0500

    compact report

--- Code.js
+++ Code.js
@@ -1,5 +1,3 @@
-//import 'google-apps-script'
-
 function onFormSubmit(event) {
     // Locate the drive folder
     var folder = DriveApp.getFolderById(folderId);
@@ -28,15 +26,16 @@ function shareReport(report, psycho) {
 
     report.addEditor(email);
 
+    //var pdf = report.getAs(MimeType.PDF);
+    //folder.createFile(pdf).setName(fileName);
+    //GmailApp.sendEmail('foo@example.com', 'answers', 'See answers attached ', {attachments: pdf});
+
     GmailApp.sendEmail(email, report.getName(), 'See answers at ' + report.getUrl(), {});
 }
 
 
 function renderForm(form, response, folder) {
 
-    //var firstName = getResponse(response, 'First Name');
-    //var lastName = getResponse(response, 'Last Name');
-    //var fileName = 'Form responses for ' + firstName + ' ' + lastName;
     var fileName = 'Form responses';
 
     // Create a new report
@@ -47,6 +46,9 @@ function renderForm(form, response, folder) {
     var file = DriveApp.getFileById(report.getId());
     folder.addFile(file);
     DriveApp.getRootFolder().removeFile(file);
+
+    repbody.appendParagraph('Answers: ')
+           .setHeading(DocumentApp.ParagraphHeading.HEADING2);
 
     // Itererate over element in the form
     var formItems = form.getItems();
@@ -89,14 +91,38 @@ function renderForm(form, response, folder) {
     // Remove empty line at the beginning of the document
     repbody.removeChild(repbody.getChild(0));
 
-    // Save report
+    repbody.appendPageBreak();
+    repbody.appendParagraph('Questions: ')
+           .setHeading(DocumentApp.ParagraphHeading.HEADING2);
+
+    // Itererate over element in the form
+    var formItems = form.getItems();
+    var questionCount = 0;
+    for (var i = 0, j = 0; i < formItems.length; i++) {
+        var formItem = formItems[i];
+        switch (formItem.getType()) {
+            // Ignore unsupported elements
+            default:
+                break;
+            // Supported question types
+            case FormApp.ItemType.CHECKBOX:
+            case FormApp.ItemType.CHECKBOX_GRID:
+            case FormApp.ItemType.GRID:
+            case FormApp.ItemType.LIST:
+            case FormApp.ItemType.MULTIPLE_CHOICE:
+            case FormApp.ItemType.SCALE:
+            case FormApp.ItemType.TEXT:
+            case FormApp.ItemType.PARAGRAPH_TEXT:
+            case FormApp.ItemType.DATE:
+            case FormApp.ItemType.TIME:
+                questionCount++;
+                repbody.appendParagraph(questionCount.toString() + '. ' + formItem.getTitle())
+                       .setFontSize(8);
+                break;
+        }
+    }
+
     report.saveAndClose();
-
-    //var pdf = report.getAs(MimeType.PDF);
-    //folder.createFile(pdf).setName(fileName);
-
-    //GmailApp.sendEmail('foo@example.com', 'answers', 'See answers attached ', {attachments: pdf});
-
     return report;
 }
 
@@ -115,206 +141,95 @@ function getResponse(response, questionText) {
 }
 
 
-// Add the question title
-function addQuestionHeader(repbody, question) {
-    repbody.appendParagraph('Question: ' + question.getTitle())
-           .setHeading(DocumentApp.ParagraphHeading.HEADING3)
-           .setBold(true);
-}
-
-
 function addQuestionCheckboxGrid(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem('Checkbox Grid:').setBold(false);
     var questionRows = question.getRows();
-    var questionCols = question.getColumns();
-    var answers = responseItem ? responseItem.getResponse() : [[]];
-
-    var tableCells = [];
-
-    // Top header row:  ' ' , col1, col2, col3...
-    var header = [''].concat(questionCols);
-    tableCells.push(header);
-
-    // Rows:
+    var answers = responseItem.getResponse();
     for (var i = 0; i < questionRows.length; i++) {
         var row = [questionRows[i]];
-        for (var j = 0; j < questionCols.length; j++) {
-            var found = false;
-            if (answers[i]) {
-                for (var a = 0; a < answers[i].length; a++) {
-                    if (answers[i][a] == questionCols[j]) {
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            if (found) {
-                row.push('✅');
-            } else {
-                row.push('🔲');
-            }
+        var text = row.length < 35 ? row : row.substr(0, 32) + '...';
+        if (answers[i]) {
+            text += ": ✅ " + answers[i].join(', ');
+        } else {
+            text += ": N/A";
         }
-        tableCells.push(row);
+        repbody.appendListItem(text)
+               .setNestingLevel(1)
+               .setIndentStart(72)
+               .setGlyphType(DocumentApp.GlyphType.SQUARE_BULLET);
     }
 
-    var table = repbody.appendTable(tableCells);
-    alignInnerCells(table, questionRows.length, questionCols.length);
-}
-
-
-function alignInnerCells(table, rowCount, colCount) {
-    var style = {};
-    style[DocumentApp.Attribute.HORIZONTAL_ALIGNMENT] = DocumentApp.HorizontalAlignment.CENTER;
-    for (var i = 0; i < rowCount; i++) {
-        for (var j = 0; j < colCount; j++) {
-            var cell = table.getCell(i + 1, j + 1);
-            var firstChild = cell.getChild(0);
-            if (firstChild.getType() == DocumentApp.ElementType.PARAGRAPH) {
-                firstChild.asParagraph().setAttributes(style);
-            }
-        }
-    }
 }
 
 
 function addQuestionGrid(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem('Multiple Choice Grid:').setBold(false);
     var questionRows = question.getRows();
-    var questionCols = question.getColumns();
-    var answers = responseItem ? responseItem.getResponse() : [];
-
-    var tableCells = [];
-
-    // Top header row:  ' ' , col1, col2, col3...
-    var header = [''].concat(questionCols);
-    tableCells.push(header);
-
-    // Rows:
+    var answers = responseItem.getResponse();
     for (var i = 0; i < questionRows.length; i++) {
         var row = [questionRows[i]];
-        for (var j = 0; j < questionCols.length; j++) {
-            if (answers[i] == questionCols[j]) {
-                row.push('⦿');
-            } else {
-                row.push('◦');
-            }
+        var text = row.length < 35 ? row : row.substr(0, 32) + '...';
+        if (answers[i]) {
+            text += ": ⦿ " + answers[i];
+        } else {
+            text += ": N/A";
         }
-        tableCells.push(row);
+        repbody.appendListItem(text)
+               .setNestingLevel(1)
+               .setIndentStart(72)
+               .setGlyphType(DocumentApp.GlyphType.SQUARE_BULLET);
     }
-
-    var table = repbody.appendTable(tableCells);
-    alignInnerCells(table, questionRows.length, questionCols.length);
-}
-
-
-function addQuestionDate(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-    repbody.appendParagraph('ERROR: date not yet supported').setBold(false);
-}
-
-
-function addQuestionTime(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-    repbody.appendParagraph('ERROR: time not yet supported').setBold(false);
 }
 
 
 function addQuestionList(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-    var choices = question.getChoices();
-    var responseText = responseItem ? responseItem.getResponse() : '';
-    for (const choice of choices) {
-        var choiceText = choice.getValue();
-        if (responseText == choiceText) {
-            repbody.appendParagraph(choiceText + ' (*)').setBold(true);
-        }
-        else {
-            repbody.appendParagraph(choiceText).setBold(false);
-        }
-    }
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem("➡ " + responseItem.getResponse()).setBold(false);
 }
 
 
 function addQuestionCheckbox(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-
-    var choices = question.getChoices();
-    var responseList = responseItem ? responseItem.getResponse() : [];
-    // Add each box tat was checked
-    for (const choice of choices) {
-        var choiceText = choice.getValue();
-        var bullet = '🔲';
-        var found = false;
-        for (const responseText of responseList) {
-            if (responseText == choiceText) {
-                bullet = '✅';
-                found = true;
-                break;
-            }
-        }
-        repbody.appendParagraph(bullet + ' ' + choiceText).setBold(found);
-    }
     if (!responseItem) {
+        repbody.appendListItem("N/A");
         return;
-    }
-    // Detect and add the 'Other' box
-    for (const responseText of responseList) {
-        var found = false;
-        for (const choice of choices) {
-            var choiceText = choice.getValue();
-            if (responseText == choiceText) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            repbody.appendParagraph("✅ Other: " + responseText).setBold(true);
-        }
-    }
+    }    
+    repbody.appendListItem("✅ " + responseItem.getResponse().join(', ')).setBold(false);
 }
 
 
 function addQuestionMultipleChoice(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-
-    var choices = question.getChoices();
-    var responseText = responseItem ? responseItem.getResponse() : '';
-    var found = false;
-    for (const choice of choices) {
-        var choiceText = choice.getValue();
-        var bullet = '◦';
-        var bold = false;
-        if (responseText == choiceText) {
-            bullet = '⦿';
-            found = true;
-            bold = true;
-        }
-        repbody.appendParagraph(bullet + ' ' + choiceText).setBold(bold);
-    }
-    if (responseItem && !found) {
-        repbody.appendParagraph("⦿ Other: " + responseText).setBold(true);
-    }
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem("⦿ " + responseItem.getResponse()).setBold(false);
 }
 
 
 function addQuestionText(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-    var responseText = responseItem ? responseItem.getResponse() : '';
-    repbody.appendParagraph("➡ " + responseText).setBold(false);
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem(responseItem.getResponse()).setBold(false);
 }
 
 
 function addQuestionScale(repbody, question, responseItem) {
-    addQuestionHeader(repbody, question);
-    var responseText = responseItem ? responseItem.getResponse() : '';
-    var steps = [];
-    for (var i = question.getLowerBound(); i <= question.getUpperBound(); i++) {
-        if (i == responseText) {
-            steps.push('⦿ ' + i);
-        } else {
-            steps.push('◦ ' + i);
-        }
-
-    }
-    repbody.appendParagraph(steps.join('    ')).setBold(false);
+    if (!responseItem) {
+        repbody.appendListItem("N/A");
+        return;
+    }    
+    repbody.appendListItem("⦿ " + responseItem.getResponse()).setBold(false);
 }
---
+++ Vars.js
@@ -0,0 +1,3 @@
+var folderId = '1l1PNYtABFmZyZTI-4gqLGNyg1OwNMqm7';
+var spreadSheetId = '1SNMlHuP_K-5sJW-9WNwLFeUPFFc8qBCe0ODzd9RRuS0';
+var psychTitle = 'Psycho';
